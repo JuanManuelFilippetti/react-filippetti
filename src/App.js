92269457924b0f6082b8c2a10b95938ed41631a1
filippettijuanm <filippetti_juanm@hotmail.com>
2023-03-17 19:21:16 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <itemListContainer greeting={'Bienvenidos'}/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,22 @@
 import './Navbar.css'
-import logo from './assets/cart.svg'
+//import logo from './assets/cart.svg'
+import CartWidget from '../../CartWidget/CartWidget'
 
 const Navbar = () => {
     return (
-        <nav className="Navbar">
-            <h1>Ecommerce</h1>
-            <div>
+        <nav>
+            <div className="Navbar">
+                <h1>Ecommerce</h1>
                 <button>Juguetes de Madera</button>
                 <button>Didacticos</button>
                 <button>Laberintos</button>
             </div>
+            <div className="cart">
+            <CartWidget />
+            </div>
         </nav>
     )
 }
+
 
 export default Navbar
