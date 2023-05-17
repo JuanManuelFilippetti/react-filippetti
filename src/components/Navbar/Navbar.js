170c24dd76f4581b0d01451b47cee04f9554a4ad
filippettijuanm <filippetti_juanm@hotmail.com>
2023-05-16 23:45:12 -0300

    correccion4

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -25,7 +25,7 @@ const NavBar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid" >
-      <a class="navbar-brand" href="http://localhost:3000"><img src={logo} /></a>
+      <a class="navbar-brand" href="http://localhost:3000">Home</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
